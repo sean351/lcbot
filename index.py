e60fb15f82ede484a70cfc50a6d12a34042149a9
Sean Doran <sdoran35@gmail.com>
2024-03-14 15:51:30 -0400

    fix port

--- index.py
+++ index.py
@@ -388,6 +388,6 @@ async def on_ready():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
     client.run(os.environ.get('DISCORD_BOT_TOKEN'))
